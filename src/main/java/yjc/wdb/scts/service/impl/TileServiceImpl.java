861e21223b6d0b096ddccad0ccd8c99b936497fa
hs961231 <hs961231@naver.com>
2017-08-07 17:55:20 +0900

    2017_08_07 정혜수  타일별로 이제 리스트만 뿌리면 됨

--- src/main/java/yjc/wdb/scts/service/impl/TileServiceImpl.java
+++ src/main/java/yjc/wdb/scts/service/impl/TileServiceImpl.java
@@ -71,40 +71,75 @@ public class TileServiceImpl implements TileService {
 		
 		String[] user_group = {"10/m/yes", "10/m/no", "10/w/no", "10/w/yes", "20/m/yes", "20/m/no", "20/w/no", "20/w/yes", "30/m/yes", "30/m/no", "30/w/no", "30/w/yes", "40/m/yes", "40/m/no", "40/w/no", "40/w/yes", "50/m/yes", "50/m/no", "50/w/no", "50/w/yes"};
 		
-		System.out.println(user_group.length);
 		
-		/*List<HashMap> avgList = tiledao.avgStayTime(drw_code, tile_crdnt_x, tile_crdnt_y);
+		
+		List<HashMap> avgList = tiledao.avgStayTime(drw_code, tile_crdnt_x, tile_crdnt_y);
 		List<HashMap<String, String>> avg = new ArrayList<HashMap<String, String>>();
 		
 		int count = 0;
+
 		for(int i = 0; i < user_group.length; i++){
-			System.out.println("여기옴 33333");
+			count = 0;
+
 			for(int j = 0; j < avgList.size(); j++){
 				
-				if(user_group[i].equals(avgList.get(i).get("user_group").toString())){
+				if(user_group[i].equals(avgList.get(j).get("user_group").toString())){
+	
 					HashMap map = new HashMap();
-					map.put("user_group", avgList.get(i).get("user_group"));
-					map.put("avgStayTime", avgList.get(i).get("avgStayTime"));
-
-					avg.add(map);
-				}else{
-					HashMap map = new HashMap();
-					map.put("user_group", user_group[i]);
-					map.put("avgStayTime", "0");
-					avgList.add(map);
+					map.put("user_group", avgList.get(j).get("user_group"));
+					map.put("avgStayTime", avgList.get(j).get("avgStayTime"));
 					count++;
+					avg.add(map);
 				}
 				
-				if(count == 1){
-					break;
-				}
+			}
+			
+			if(count == 0){
+				HashMap map = new HashMap();
+				map.put("user_group", user_group[i]);
+				map.put("avgStayTime", "0");
+				
+				avg.add(map);
+		
 			}
 		}
 		
 		System.out.println("와아아아아  "+avg.toString());
 		
 		List<HashMap> goodsList = tiledao.tile_goods(drw_code, tile_crdnt_x, tile_crdnt_y);
-		*/
+		List<HashMap<String, String>> goods = new ArrayList<HashMap<String, String>>();
+		
+		for(int i = 0; i < user_group.length; i++){
+			count = 0;
+
+			for(int j = 0; j < goodsList.size(); j++){
+				
+				if(user_group[i].equals(goodsList.get(j).get("user_group").toString())){
+	
+					HashMap map = new HashMap();
+					map.put("user_group", goodsList.get(j).get("user_group"));
+					map.put("totalPrice", goodsList.get(j).get("totalPrice"));
+					count++;
+					goods.add(map);
+				}
+				
+			}
+			
+			if(count == 0){
+				HashMap map = new HashMap();
+				map.put("user_group", user_group[i]);
+				map.put("totalPrice", "0");
+				
+				goods.add(map);
+		
+			}
+		}
+		
+		System.out.println("와아아아아dfadefadfdfdf  "+goods.toString());
+		
+		json.put("tile_goods", goods);
+		json.put("avgStay", avg);
+
 		return json;
 	}
 
